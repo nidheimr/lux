5740fa0296b6ff178d36db43d4ea6ce84ca87ccb
nidheimr <aldrnidheimr@gmail.com>
2025-04-05 14:15:40 +0100

    actually used the loaded gl functions to create a triangle

--- tests/main.c
+++ tests/main.c
@@ -1,16 +1,100 @@
 #include "lux.h"
 
+const char* vertex_source = "#version 330 core      \n\
+layout (location = 0) in vec3 pos;                  \n\
+void main()                                         \n\
+{                                                   \n\
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0f);  \n\
+}";
+
+const char* fragment_source = "#version 330 core    \n\
+out vec4 FragColor;                                 \n\
+void main()                                         \n\
+{                                                   \n\
+    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);       \n\
+}";
+
 int main()
 {
     lx_enable_debug_messages(1);
     
     lx_window* window = lx_window_new("Lux", 1920, 1080);
 
+    int success;
+    char info[512];
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+    glCompileShader(vertex_shader);
+
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info);
+        lx_error("opengl error: %s", info);
+    }
+
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info);
+        lx_error("opengl error: %s", info);
+    }
+
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, info);
+        lx_error("opengl error: %s", info);
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    float vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (!lx_window_has_received_quit_signal(window))
     {
         lx_window_update(window);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         lx_window_render(window);
     }
+
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteProgram(shader_program);
 
     lx_window_delete(window);
 
