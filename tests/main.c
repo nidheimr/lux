9fee8fc80bb7b77571cf0a86e288edaf4290202f
nidheimr <aldrnidheimr@gmail.com>
2025-04-23 19:21:30 +0100

    test: messing around with attrib pointers

--- tests/main.c
+++ tests/main.c
@@ -1,17 +1,21 @@
 #include "lux.h"
 
-const char* vertex_source = "#version 330 core      \n\
-layout (location = 0) in vec3 pos;                  \n\
-void main()                                         \n\
-{                                                   \n\
-    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0f);  \n\
+const char* vertex_source = "#version 330 core          \n\
+layout (location = 0) in vec3 pos;                      \n\
+layout (location = 1) in vec3 col;                      \n\
+out vec3 d_col;                                         \n\
+void main()                                             \n\
+{                                                       \n\
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0f);      \n\
+    d_col = col;                                        \n\
 }";
 
-const char* fragment_source = "#version 330 core    \n\
-out vec4 FragColor;                                 \n\
-void main()                                         \n\
-{                                                   \n\
-    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);       \n\
+const char* fragment_source = "#version 330 core        \n\
+in vec3 d_col;                                          \n\
+out vec4 FragColor;                                     \n\
+void main()                                             \n\
+{                                                       \n\
+    FragColor = vec4(d_col.x, d_col.y, d_col.z, 1.0f);  \n\
 }";
 
 int main()
@@ -23,9 +27,10 @@ int main()
     
     float vertices[] =
     {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        // xyz                  // rgb
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f
     };
 
     unsigned int vao, vbo;
@@ -36,8 +41,12 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
