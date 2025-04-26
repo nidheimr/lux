2df21ece6b44c0e611c153b64b83ed01576b3a32
nidheimr <aldrnidheimr@gmail.com>
2025-04-26 23:24:36 +0100

    slow rewrite

---
+++ include/lux/render.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//
+//  window
+//
+
+typedef struct _lx_window lx_window;
+
+lx_window* lx_window_new(const char* title, int width, int height);
+void lx_window_delete(lx_window* window);
+
+void lx_window_update(lx_window* window);
+void lx_window_render(lx_window* window);
+
+int lx_window_has_received_close_signal(lx_window* window);
--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,18 @@
+#include "lux/render.h"
 #include <lux.h>
 
 int main()
 {
     lx_enable_debug_messages(1);
+    lx_window* window = lx_window_new("Lux", 1920, 1080);
+
+    while (lx_window_has_received_close_signal(window))
+    {
+        lx_window_update(window);
+        lx_window_render(window);
+    }
+
+    lx_window_delete(window);
 
     return 0;
 }
