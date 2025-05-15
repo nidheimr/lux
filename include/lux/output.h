612b9bc3499770f137e9d73d30263597c2c33b7f
nidheimr <aldrnidheimr@gmail.com>
2025-05-15 20:28:48 +0100

    i documentated badly, oops

--- include/lux/output.h
+++ include/lux/output.h
@@ -24,7 +24,7 @@ typedef struct _lx_window lx_window;
 lx_window* lx_window_create(const char* title, int width, int height);
 
 /**
- * Properly closes the window and frees any memory associated with it. The
+ * @brief Properly closes the window and frees any memory associated with it. The
  * window cannot be used after calling this.
  *
  * @param window The pointer to the window.
@@ -32,7 +32,7 @@ lx_window* lx_window_create(const char* title, int width, int height);
 void lx_window_destroy(lx_window* window);
 
 /**
- * Polls the window events and processes any input received. then proceeds
+ * @brief Polls the window events and processes any input received. then proceeds
  * to clear the back buffer ready for drawing.
  *
  * Though not required, it is recommended to call this function **before** a
@@ -43,7 +43,7 @@ void lx_window_destroy(lx_window* window);
 void lx_window_update(lx_window* window);
 
 /**
- * Swaps the front and back buffers of the window, presenting whatever was
+ * @brief Swaps the front and back buffers of the window, presenting whatever was
  * drawn on the back buffer. 
  *
  * Though not required, it is recommended to call this function **after** a
@@ -54,7 +54,7 @@ void lx_window_update(lx_window* window);
 void lx_window_render(lx_window* window);
 
 /**
- * Used to detect if a quit signal has been given to the window either through
+ * @brief Used to detect if a quit signal has been given to the window either through
  * the user trying to close the window or through signals.
  *
  * @param window The pointer to the window.
