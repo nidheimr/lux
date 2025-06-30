d88ffd15041094f3314433e596f2e58925c676d0
nidheimr <aldrnidheimr@gmail.com>
2025-06-30 17:30:49 +0100

    i forgot to update the tests

--- tests/src/main.c
+++ tests/src/main.c
@@ -5,21 +5,21 @@ int main()
     lx_enable_debug_messages(1);
     lx_print_error_on_occurance(1);
 
-    lx_window_create("Lux Window", 1920, 1080); 
+    lx_window* window = lx_window_create("Lux Window", 1920, 1080); 
     lx_shader shader = lx_shader_create("default.vert", "default.frag");
 
-    while (lx_window_is_alive())
+    while (lx_window_is_alive(window))
     {
-        lx_window_update();
+        lx_window_update(window);
         
         if (lx_key_is_down(LX_KEY_A))
             lx_shader_test(shader);
 
-        lx_window_render();
+        lx_window_render(window);
     }
 
     lx_shader_destroy(shader);
-    lx_window_destroy();
+    lx_window_destroy(window);
 
     return 0;
 }
